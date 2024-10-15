c431661f9b02ba743297db14472cfec80d023c5b
FerSousa7 <fernandoti767@gmail.com>
2024-10-15 14:57:43 -0300

    Criando repositório de teste

---
+++ 1-MeuPriemeiroProjeto.js
@@ -0,0 +1,12 @@
+console.log("Meu Primeiro programa: trabalhando com variaveis");
+
+const idade = 26;
+
+console.log(idade);
+console.log(idade);
+console.log(idade - 2);
+console.log(idade / 2);
+console.log(idade + 2);
+
+const idadeSomada = idade + 2;
+console.log(idadeSomada);
---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,7 @@
+//Console.log("trabalhando com variveis");
+console.log("trabalhando com variveis");
+//Js é case sensitive
+
+const idade = 18;
+const name = "Fernando";
+let ano = 2024;
